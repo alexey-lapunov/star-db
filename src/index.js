11ee4007adb3340b5d847517d18d88e7cfeb5890
Alexey <leshka.lapunov@gmail.com>
2019-06-06 02:37:35 +0300

    bugfix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-<<<<<<< HEAD
-import App from './app/containers/App';
+import App from './components/App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
-
-=======
-import App from './components/App/';
-
-ReactDOM.render(<App/>, document.getElementById('root'));
->>>>>>> master
